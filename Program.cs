d449f7bf648221bc603d543f8fb041b8cf277190
Jack Gittoes <Jrgittoes@gmail.com>
2020-06-29 12:06:45 +0100

    comment

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ class MainClass
         {
             return 1;
         }
+        // e.g. If num == 4 then 4*3*2*1. The program multiplies num * --num till it can't multiply anymore 
         else if (num > 1)
         {
             return num * FirstFactorial(--num);
