b3214ac3f0f921bcd288cf75e663fc230e5aadce
Jack Gittoes <Jrgittoes@gmail.com>
2020-06-29 12:03:53 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class MainClass
+{
+
+    public static int FirstFactorial(int num)
+    {
+
+        if (num < 1)
+        {
+            return 1;
+        }
+        else if (num > 1)
+        {
+            return num * FirstFactorial(--num);
+        }
+
+        return num;
+    }
+
+    static void Main()
+    {
+        // keep this function call here
+        Console.WriteLine(FirstFactorial(Convert.ToInt32(Console.ReadLine())));
+    }
+
+}
